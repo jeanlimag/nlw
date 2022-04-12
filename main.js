68163a6c2abf2da4421c987f83158811fe893d98
jeanlimag <86786213+jeanlimag@users.noreply.github.com>
2022-04-12 12:25:41 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,91 @@
+//Toggle menu//
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+const links = document.querySelectorAll('nav ul li a')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+/*Scroll Header*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+function changeHeaderWhenScrool() {
+  if (this.window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+
+/*Testimonials Slider*/
+const swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+/* Scroll Reveal */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+scrollReveal.reveal(
+  `#home .image, #home .text, #about .image, #about .text, #services header, #services .card, #testimonials header, #testimonials .testimonials, #contact .text, #contact .links, footer .brand, footer .social`,
+  { interval: 100 }
+)
+/* Botão Back to top */
+const backToTopButton = document.querySelector('.back-to-top')
+function backToTop() {
+  if (this.window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+/* Menu ativo*/
+const sections = document.querySelectorAll('main section[id]')
+function activateMenu() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+/* Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScrool()
+  backToTop()
+  activateMenu()
+})
